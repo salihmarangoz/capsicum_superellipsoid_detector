28eecda6a4c9937ee843ee8acac7a7572caad1d8
salihmarangoz <salih285@gmail.com>
2022-02-08 05:27:41 +0100

    added experimental clustering method

--- include/capsicum_superellipsoid_detector/clustering.h
+++ include/capsicum_superellipsoid_detector/clustering.h
@@ -7,12 +7,20 @@
 #include <pcl/search/search.h>
 #include <pcl/search/kdtree.h>
 #include <pcl/segmentation/extract_clusters.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/segmentation/conditional_euclidean_clustering.h>
 
 namespace clustering
 {
 
 std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> 
 euclideanClusterExtraction(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in, std::vector<pcl::PointIndices> &cluster_indices_out, float cluster_tolerance, int min_cluster_size, int max_cluster_size);
+
+std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr>
+experimentalClustering(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in, std::vector<pcl::PointIndices> &cluster_indices_out);
+
+bool
+experimentalClusteringCondition(const pcl::PointXYZRGBNormal& point_a, const pcl::PointXYZRGBNormal& point_b, float squared_distance);
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr 
 getColoredCloud (pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in, std::vector<pcl::PointIndices> &cluster_indices_in);
--- src/clustering.cpp
+++ src/clustering.cpp
@@ -35,6 +35,74 @@ euclideanClusterExtraction(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in, std:
   return all_clusters;
 }
 
+std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr>
+experimentalClustering(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in, std::vector<pcl::PointIndices> &cluster_indices_out)
+{
+  pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
+  tree->setInputCloud (cloud_in);
+  pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+  pcl::copyPointCloud (*cloud_in, *cloud_with_normals);
+
+  // Compute normals
+  pcl::NormalEstimation<pcl::PointXYZRGB, pcl::PointXYZRGBNormal> ne;
+  ne.setInputCloud (cloud_in);
+  ne.setSearchMethod (tree);
+  ne.setRadiusSearch (0.03);
+  ne.compute(*cloud_with_normals);
+
+  // Set up a Conditional Euclidean Clustering class
+  pcl::ConditionalEuclideanClustering<pcl::PointXYZRGBNormal> cec (true);
+  cec.setInputCloud (cloud_with_normals);
+  cec.setConditionFunction (&experimentalClusteringCondition);
+  cec.setClusterTolerance (0.02);
+  cec.setMinClusterSize (100);
+  cec.setMaxClusterSize (10000);
+  cec.segment (cluster_indices_out);
+  //cec.getRemovedClusters (small_clusters, large_clusters);
+
+  std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> all_clusters;
+  all_clusters.reserve(cluster_indices_out.size());
+
+  for (const auto& i_segment : cluster_indices_out)
+  {
+    // Gather points for a segment
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr current_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
+    current_cluster->reserve(i_segment.indices.size());
+    for (const auto& i_point : (i_segment.indices))
+    {
+      current_cluster->push_back(cloud_in->at(i_point));
+    }
+    all_clusters.push_back(current_cluster);
+  }
+
+  return all_clusters;
+}
+
+bool
+experimentalClusteringCondition(const pcl::PointXYZRGBNormal& point_a, const pcl::PointXYZRGBNormal& point_b, float squared_distance)
+{
+  /*
+  Eigen::Map<const Eigen::Vector3f> point_a_normal = point_a.getNormalVector3fMap(), point_b_normal = point_b.getNormalVector3fMap();
+  printf("%f ", std::abs(point_a_normal.dot(point_b_normal)));
+  if (std::abs(point_a_normal.dot(point_b_normal)) > 0.9)
+  {
+    return true;
+  }
+  return false;
+  */
+
+  Eigen::Map<const Eigen::Vector3f> point_a_normal = point_a.getNormalVector3fMap(), point_b_normal = point_b.getNormalVector3fMap();
+  Eigen::Vector3f d = point_a.getVector3fMap() - point_b.getVector3fMap();
+  d.normalize();
+
+  printf("%f ", std::abs(d.dot(point_a_normal)) + std::abs(d.dot(point_b_normal)));
+  if (std::abs(d.dot(point_a_normal)) + std::abs(d.dot(point_b_normal)) < 0.1)
+  {
+    return true;
+  }
+  return false;
+}
+
 
 // copied & modified from https://pointclouds.org/documentation/region__growing_8hpp_source.html
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr 
